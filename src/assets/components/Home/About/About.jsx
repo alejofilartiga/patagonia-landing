a6b5fdb1a71d2359e6f80eab32f5949d444bb299
Alejo <alejofilartiga@gmail.com>
2025-06-05 17:17:41 -0300

    added About and Hero section in HTML

--- src/assets/components/Home/About/About.jsx
+++ src/assets/components/Home/About/About.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
 
 function About() {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <div>
+          <h2>2006</h2>
+          <h3>UN VIAJE QUE COMENZO EN LA PATAGONIA</h3>
+          <h2>2025</h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <img src="./img/tree.png" alt="" />
+          </div>
+          <h2>ORIGEN</h2>
+          <p>En el corazón de la Patagonia, un grupo de amigos apasionados por la cerveza artesanal comenzó a experimentar con ingredientes locales para crear sabores únicos.</p>
+        </div>
+        <div>
+          <div>
+            <img src="./img/beer.png" alt="" />
+          </div>
+          <h2>EXPANSION</h2>
+          <p>Abrimos nuestro primer Refugio Cervecero en Bariloche, ofreciendo cervezas frescas en un entorno natural incomparable. Nuevos estilos y más seguidores impulsaron nuestro crecimiento.</p>
+        </div>
+        <div>
+          <div>
+            <img src="./img/world.png" alt="" />
+          </div>
+          <h2>COMPROMISO</h2>
+          <p>Lanzamos nuestro programa de reforestación y optimizamos nuestros procesos para reducir el impacto ambiental. Más que una cerveza, un compromiso con la naturaleza.</p>
+        </div>
+        <div>
+          <div>
+            <img src="./img/mountain.png" alt="" />
+          </div>
+          <h2>MAS ALLA</h2>
+          <p>Expandimos nuestros Refugios Cerveceros a nuevas ciudades, llevando el espíritu patagónico a más personas y explorando nuevos sabores.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/assets/components/Home/Hero/Hero.jsx
+++ src/assets/components/Home/Hero/Hero.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <h2>NUEVA EDICION</h2>
+        <p>Suave como tantas, rica como Patagonia.</p>
+        <button>CONSEGUILA</button>
+      </div>
+    </section>
   )
 }
 
