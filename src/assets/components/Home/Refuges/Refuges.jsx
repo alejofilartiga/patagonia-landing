1cb94e75682d0cafa53eae251230e47fe960b5ae
Alejo <alejofilartiga@gmail.com>
2025-06-07 10:00:00 -0300

    Added Refuges section HTML

--- src/assets/components/Home/Refuges/Refuges.jsx
+++ src/assets/components/Home/Refuges/Refuges.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 
 function Refuges() {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <h2>NUESTROS REFUGIOS MAS CONCURRIDOS</h2>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>Bariloche</h3>
+            <p>Km 24,7, Circuito Chico.</p>
+          </div>
+        </div>
+        
+        <div>
+          <div>
+            <h3>Palermo</h3>
+            <p>Costa Rica 4522, CABA.</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h3>Ushuaia</h3>
+            <p>Maipú 337, Ushuaia.</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h3>Iguazú</h3>
+            <p>Av. Córdoba 150, N3370 Iguazú.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
