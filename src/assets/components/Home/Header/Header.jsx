bd970dc82ed077e0e25bffb1842284599da0aece
Alejo <alejofilartiga@gmail.com>
2025-06-12 22:12:26 -0300

    Update logo Header

--- src/assets/components/Home/Header/Header.jsx
+++ src/assets/components/Home/Header/Header.jsx
@@ -3,9 +3,9 @@ import navlogo from "./img/navlogo.png";
 function Header() {
   return (
     <header className="flex bg-[#02291F] p-2  w-full font-poppins">
-      <nav className="flex justify-between items-center text-white w-full">
+      <nav className="flex justify-around items-center text-white w-full">
         <div>
-          <img className="w-40" src={navlogo} alt="Patagonia Logo" />
+          <img className="w-15" src={navlogo} alt="Patagonia Logo" />
         </div>
         <div className="flex gap-2">
           <a className="text-xs" href="#about">NOSOTROS</a>
